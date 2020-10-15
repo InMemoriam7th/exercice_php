5152933829b6f2a65a259a3a9b28e99bba894b6a
InMemoriam7th <62008705+InMemoriam7th@users.noreply.github.com>
2020-10-15 16:12:32 +0200

    formating

--- src/main_inc.php
+++ src/main_inc.php
@@ -1,8 +1,8 @@
 <?php
                         $tableau = array(
-                            1=>array("classement"=>1,"langage"=>"Python","type"=>"Web &amp; application"),
-                            2=>array("classement"=>2,"langage"=>"C++","type"=>"Application"),
-                            3=>array("classement"=>3,"langage"=>"NodeJs","type"=>"Web &amp; application"),
-                            4=>array("classement"=>4,"langage"=>"PHP","type"=>"Web &amp; application"),
+                            0=>array("classement"=>"1","langage"=>"Python","type"=>"Web & application"),
+                            1=>array("classement"=>"2","langage"=>"C++","type"=>"Application"),
+                            2=>array("classement"=>"3","langage"=>"NodeJs","type"=>"Web & application"),
+                            3=>array("classement"=>"4","langage"=>"PHP","type"=>"Web & application"),
                         );
                         ?>
--- started.php
+++ started.php
@@ -1,9 +1,13 @@
 <?php
+
     include_once("./src/head_inc.php");
+
 ?>
 <body>
     <?php
+
     include_once("./src/header_inc.php");
+    
     ?>
     <main>
         <div class="section" role="table" aria-label="table-info">
@@ -11,18 +15,23 @@
                 <caption>Le top 10 des langages</caption>
                 <thead>
                     <tr>
-                        <th role="columnheader">Classement</th>
+                        <th>Classement</th>
                         <th>Langage</th>
                         <th>Type</th>
                     </tr>
                 </thead>
                 <?php
+                
                 include_once("./src/main_inc.php");
+
                 foreach($tableau as $key_1 => $index){
                     print "<tr>\n";
+
                     foreach($index as $key_2 => $value){
                         echo "<td>".$value."</td>\n";
+
                     }
+
                     print "</tr>\n";
                 }
                 ?>
@@ -33,8 +42,14 @@
                             <th>Type</th>
                         </tr>
                 </tfoot>
+            </table>
+            </div>
                 <footer>
-                    <p>&copy; - Giusmili <?php print date('Y') ?></p>
+                    <?php
+
+                    include_once("./src/footer_inc.php");
+                    
+                    ?>
                 </footer>
             </main>
 </body>
